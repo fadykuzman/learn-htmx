4ae8828f214acc6b34085db738ebffbc53be74c0
Fady Kuzman <fady.kuzman@pm.me>
2025-01-22 22:45:46 +0100

    Finish htmx crud

--- dogs-crud/src/server.tsx
+++ dogs-crud/src/server.tsx
@@ -1,16 +1,66 @@
 import {Database, Statement} from 'bun:sqlite'
-import {Context, Hono} from 'hono'
+import {type Context, Hono} from 'hono'
 import {serveStatic} from 'hono/bun'
 
-//-------------------
-// SQL Preparation
-//-------------------
+type Dog = {id: string, name: string, breed: string}
+const dogs = new Map<string, Dog>()
 
-const db = new Database('todos.db', {create: true});
+function addDog(name: string, breed: string): Dog {
+	const id = crypto.randomUUID()
+	const dog = {id, name, breed}
+	dogs.set(id, dog)
+	return dog
+}
+
+addDog('Comet', 'Whippet');
+addDog('Oscar', 'German Shorthaired Pointer');
+
+function dogRow(dog: Dog) {
+	return (
+		<tr class="on-hover">
+		  <td>{dog.name}</td>
+		  <td>{dog.breed}</td>
+		  <td class="buttons">
+		    <button
+			  class="show-on-hover"
+			  hx-delete={`/dog/${dog.id}`}
+			  hx-confirm="Are you sure?"
+			  hx-target="closest tr"
+			  hx-swap="delete"
+			>
+			x
+			</button>
+		  </td>
+		</tr>
+	)
+}
+//-------------------
+// Server Start
+//-------------------
 
 const app = new Hono()
 
 app.use('/*', serveStatic({root: './public'}));
 
+app.get('/table-rows', (c: Context) => {
+	const sortedDogs = Array.from(dogs.values())
+	.sort((a, b) => a.name.localeCompare(b.name));
+	
+	return c.html(<>{sortedDogs.map(dogRow)}</>)
+})
+
+app.post('/dog', async (c: Context) => {
+	const formData = await c.req.formData()
+	const name = (formData.get('name') as string) || ''
+	const breed = (formData.get('breed') as string) || ''
+	const dog = addDog(name, breed)
+	return c.html(dogRow(dog), 201)
+})
+
+app.delete('/dog/:id', (c: Context) => {
+	const id = c.req.param('id')
+	dogs.delete(id)
+	return c.body(null)
+})
 
 export default app
